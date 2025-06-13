17acb2dc43656f6dc1e38fd45c25423586cdd3d7
BesarAjruli <aquila1912@gmail.com>
2025-06-13 22:13:55 +0200

    Finished design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
+import './AppStyleMobile.css'
 import Slideshow from './Slideshow'
+import Icon from '@mdi/react';
+import { mdiInstagram, mdiFacebook, mdiMagnify, mdiMenu } from '@mdi/js';
 
 function App() {
 
   return (
     <>
       <header>
-        <img src="" alt="logo" />
-        
+        <div>
+          <h1>Porositonline.gf</h1>
+          <Icon path={mdiMenu} size={1.7} className='menuIcon'></Icon>
+        </div>
+        <div>
+          <input type="search"/>
+          <Icon path={mdiMagnify} size={1.3} className='searchIcon'/>
+        </div>
+        <nav>
+          <a href="">Home</a>
+          <a href="">All Products</a>
+          <a href="">Best Seller</a>
+          <a href="">About</a>
+        </nav>
       </header>
       <main>
         <div className='artworkCont'>
@@ -30,10 +45,37 @@ function App() {
         </div>
 
         <div className='topPicks'>
-          <span>Top Pics</span>
+          <span className='tpHeader'>Top Pics</span>
           <hr />
           <Slideshow/>
         </div>
+
+        <footer>
+          <div className='footerCont'>
+            <div className='mainFooter'>
+              <span className='name'>Porositonline.gf</span>
+              <span className='footerTxt'>Your Eyewear Destination</span>
+            </div>
+            <div className='listHolder'>
+              <ul>
+                <li>+383 456 789 112</li>
+                <li>info@porositonline.gf</li>
+                <br /><br />
+                <div>
+                  <Icon path={mdiInstagram} size={1}></Icon>
+                  <Icon path={mdiFacebook} size={1}></Icon>
+                </div>
+              </ul>
+              <ul>
+              <li><a href="">Privacy Policy</a></li>
+              <li><a href="">Accessibility Statement</a></li>
+              <li><a href="">Shipping Policy</a></li>
+              <li><a href="">Terms & Conditions</a></li>
+              <li><a href="">Refound Policy</a></li>
+              </ul>
+            </div>
+          </div>
+        </footer>
       </main>
     </>
   )
